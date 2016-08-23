a18bd878f3fadcda65e1a18d1551a09d026f5a26
crashtack <crashtack@gmail.com>
2016-08-23 08:56:00 -0700

    adding some routes

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -10,5 +10,11 @@ def my_view(request):
     return Response(imported_text)
 
 
+def my_view2(request):
+    imported_text = open(os.path.join(HERE, 'sample2.html')).read()
+    return Response(imported_text)
+
+
 def includeme(config):
     config.add_view(my_view, route_name='home')
+    config.add_view(my_view2, route_name='detail')
