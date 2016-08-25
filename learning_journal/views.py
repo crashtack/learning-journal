6ce383ed804f6abfdba2bb53236b1ee68c86c1f4
crashtack <crashtack@gmail.com>
2016-08-24 17:06:20 -0700

    implemented layout.jinja2 template

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -37,46 +37,24 @@ ENTRIES = [
 
 @view_config(route_name='list', renderer='templates/home.jinja2')
 def list_(request):
-    # imported_text = open(os.path.join(HERE, 'templates/home.html')).read()
     return {"entries": ENTRIES}
 
 
 @view_config(route_name='create', renderer='templates/new-entry.jinja2')
 def create(request):
-    # imported_text = open(os.path.join(HERE, 'templates/new-entry.html')).read()
     return {"entries": ENTRIES}
 
 
 @view_config(route_name='detail', renderer='templates/single-entry.jinja2')
 def detail(request):
-    # imported_text = open(os.path.join(HERE, 'templates/single-entry.html')).read()
     return {"entries": ENTRIES}
 
 
 @view_config(route_name='update', renderer='templates/edit-entry.jinja2')
 def update(request):
-    # imported_text = open(os.path.join(HERE, 'templates/edit-entry.html')).read()
     return {"entries": ENTRIES}
 
 
 @view_config(route_name='bootstrap', renderer='templates/bootstrap.jinja2')
 def bootstrap(request):
-    # imported_text = open(os.path.join(HERE,
-    #                                   'navbar-static-top/index.html')).read()
     return {"entries": ENTRIES}
-
-
-# @view_config(route_name='list', renderer='templates/home.jinja2')
-# def bootstrap1(request):
-#     imported_text = open(os.path.join(HERE,
-#                                       'navbar-static-top/index.html')).read()
-#     return Response(imported_text)
-
-
-def includeme(config):
-    # config.add_view(list_, route_name='list')
-    config.add_view(detail, route_name='detail')
-    # config.add_view(create, route_name='create')
-    config.add_view(update, route_name='update')
-
-    # config.add_view(bootstrap, route_name='bootstrap')
