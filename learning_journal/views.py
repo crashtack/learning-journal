a77ba50946cec71baddca06f65608d2927deff29
crashtack <crashtack@gmail.com>
2016-08-25 23:45:46 -0700

    parametrized tests, all rendered pages tested

--- learning_journal/views.py
+++ learning_journal/views.py
@@ -8,30 +8,30 @@ from pyramid.httpexceptions import (HTTPNotFound, HTTPFound)
 HERE = os.path.dirname(__file__)
 
 ENTRIES = [
-     {
+    {
         "title": "Day 1",
         "id": 1,
         "date": "August 20, 2016",
         "body": "Today I learned about <strong>Pyramid</strong>."
-     },
-     {
+    },
+    {
         "title": "Day 2",
         "id": 2,
         "date": "August 21, 2016",
         "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit</b>. Curabitur sodales ligula in libero."
-     },
-     {
+    },
+    {
         "title": "Day 3",
         "id": 3,
         "date": "August 22, 2016",
         "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit</b>. Curabitur sodales ligula in libero."
-     },
-     {
+    },
+    {
         "title": "Day 4",
         "id": 4,
         "date": "August 23, 2016",
         "body": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit</b>. Curabitur sodales ligula in libero."
-     },
+    },
 ]
 
 
@@ -43,14 +43,15 @@ ENTRIES = [
 def list_(request):
     return {"entries": ENTRIES}
 
+
 @view_config(route_name='detail', renderer='templates/single-entry.jinja2')
 @view_config(route_name='update', renderer='templates/edit-entry.jinja2')
 def detail(request):
     for entry in ENTRIES:
         if entry['id'] == int(request.matchdict['id']):
             return {"entry": entry}
-        else:
-            raise HTTPNotFound()
+    else:
+        raise HTTPNotFound('Entry Not Found')
 
 
 @view_config(route_name='bootstrap', renderer='templates/bootstrap.jinja2')
